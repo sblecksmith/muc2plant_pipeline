9763a5164597eb815fc17838c48a41e970c731ed
Sarah Blecksmith <sbleck@hive-dc-7-7-64.hive.hpc.ucdavis.edu>
2026-04-09 14:55:47 -0700

    changed name of directory to search for files

--- scripts/make_sample_sheet.sh
+++ scripts/make_sample_sheet.sh
@@ -3,7 +3,7 @@
  echo -e "sample_name\tlong_sample\tr1_path\tr2_path" > sample_sheet1.txt
 
 # Add all your samples (this will extract the sample name from the filename)
- for r1 in fastq_files1/*_R1_001.fastq.gz; do
+ for r1 in fastq_files/*_R1_001.fastq.gz; do
      r2="${r1/_R1/_R2}"
      sample=$(basename "$r1" | sed 's/_R1_001.fastq.gz//')
      long_sample=$(basename "$r1" | sed 's/_R1_001.fastq.gz//')
@@ -11,4 +11,4 @@
  done
  
 # Check the first few lines
-head sample_sheet1.txt
+head sample_sheet.txt
