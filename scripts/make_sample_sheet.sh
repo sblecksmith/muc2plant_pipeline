63ca8a35743246c67242742c6b1d78c10a1b1165
Sarah Blecksmith <sbleck@hive-dc-7-7-69.hive.hpc.ucdavis.edu>
2026-04-10 10:29:15 -0700

    added comment about filename pattern matching

--- scripts/make_sample_sheet.sh
+++ scripts/make_sample_sheet.sh
@@ -1,13 +1,14 @@
 # run this in the terminal to make a sample sheet
 # Create the sample sheet from your existing files
- echo -e "sample_name\tlong_sample\tr1_path\tr2_path" > sample_sheet1.txt
+ echo -e "sample_name\tlong_sample\tr1_path\tr2_path" > sample_sheet.txt
 
 # Add all your samples (this will extract the sample name from the filename)
- for r1 in fastq_files/*_R1_001.fastq.gz; do
+# The pattern to match (*_R1.fastq.gz) may need to change for your file names
+ for r1 in fastq_files/*_R1.fastq.gz; do
      r2="${r1/_R1/_R2}"
-     sample=$(basename "$r1" | sed 's/_R1_001.fastq.gz//')
-     long_sample=$(basename "$r1" | sed 's/_R1_001.fastq.gz//')
-     echo -e "${sample}\t${long_sample}\t${r1}\t${r2}" >> sample_sheet1.txt
+     sample=$(basename "$r1" | sed 's/_R1.fastq.gz//')
+     long_sample=$(basename "$r1" | sed 's/_R1.fastq.gz//')
+     echo -e "${sample}\t${long_sample}\t${r1}\t${r2}" >> sample_sheet.txt
  done
  
 # Check the first few lines
