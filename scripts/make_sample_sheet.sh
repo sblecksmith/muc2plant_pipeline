10a6eb6708ae9425031774f8b07c5ab8dde456be
Sarah Blecksmith <sbleck@hive-dc-7-7-18.hive.hpc.ucdavis.edu>
2026-04-13 08:31:04 -0700

    updated fastq pattern match to be more universal

--- scripts/make_sample_sheet.sh
+++ scripts/make_sample_sheet.sh
@@ -3,11 +3,11 @@
  echo -e "sample_name\tlong_sample\tr1_path\tr2_path" > sample_sheet.txt
 
 # Add all your samples (this will extract the sample name from the filename)
-# The pattern to match (*_R1.fastq.gz) may need to change for your file names
- for r1 in fastq_files/*_R1.fastq.gz; do
+# The pattern to match (_S[0-9]*_L[0-9]*_R1_001.fastq.gz) may need to change for your file names
+ for r1 in fastq_files/*_R1_001.fastq.gz; do
      r2="${r1/_R1/_R2}"
-     sample=$(basename "$r1" | sed 's/_R1.fastq.gz//')
-     long_sample=$(basename "$r1" | sed 's/_R1.fastq.gz//')
+     sample=$(basename "$r1" | sed 's/_S[0-9]*_L[0-9]*_R1_001.fastq.gz//')
+     long_sample=$(basename "$r1" | 's/_R1_001.fastq.gz//')
      echo -e "${sample}\t${long_sample}\t${r1}\t${r2}" >> sample_sheet.txt
  done
  
